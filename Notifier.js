b6ea4e367662191cc1f3ff1c79ec6122b1388701
Sysinfocus <info@sysinfocus.com>
2025-01-12 11:05:50 +0530

    slider issue fixed, ai chat added

---
+++ Notifier.js
@@ -0,0 +1,22 @@
+﻿const channel = new BroadcastChannel("customNotifier");
+let globalData = "";
+
+function notifyMessage(data) {
+    console.log("Message send: " + data);
+    channel.postMessage(data);
+}
+
+channel.onmessage = function (event) {
+    console.log("Message received: " + event.data);
+    getMessage(event.data);
+}
+
+function getMessage(data) {
+    const div = document.getElementById("mydiv");
+    globalData = data;
+    let e = new CustomEvent("notify", {
+        bubbles: true,
+        detail: { data: () => globalData }
+    });
+    div.dispatchEvent(e);
+}
